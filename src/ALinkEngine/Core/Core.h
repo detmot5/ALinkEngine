7cf2b3bca5947bf7881d8c736e21ecec99c442fc
Norbert Bielak <norbertbielak@gmail.com>
2021-02-25 22:48:12 +0100

    Added asserts

--- src/ALinkEngine/Core/Core.h
+++ src/ALinkEngine/Core/Core.h
@@ -9,7 +9,16 @@
 #error This day ALink keeps Apple away!
 #endif
 
+#ifdef ALINK_DEBUG
 #include <csignal>
 #define ALINK_DEBUGBREAK() raise(SIGTRAP)
+#define NL
+#define ALINK_ASSERT(x, ...)         if (!(x)) ALINK_LOG_ERROR("Alink assertion failed!"); ALINK_LOG_ERROR(__VA_ARGS__)
+#define ALINK_ENGINE_ASSERT(x, ...)  if (!(x)) { \
+                                   ALINK_LOG_ERROR("Alink enigne assertion failed!"); ALINK_LOG_ERROR(__VA_ARGS__);}
+                    
+#else 
+
+#endif
 
 #endif  // ALINKENGINE_CORE_CORE_H_
