28b7be8395fbb7daacf0ffb424b2449a7b10e9d2
Norbert Bielak <norbertbielak@gmail.com>
2021-02-25 22:49:00 +0100

    test asserts

--- src/ALinkSandbox/Sandbox.cpp
+++ src/ALinkSandbox/Sandbox.cpp
@@ -5,12 +5,14 @@
 class SandBox : public ALinkEngine::ALinkApplication {
  public:
   void Init() override { 
-    ALINK_LOG_WARN("Guguguwno %d", 20);
-    ALINK_LOG_ERROR("Guguguwno %d", 50);
+    ALINK_LOG_WARN("tessst %d", 20);
+    ALINK_LOG_ERROR("Testtt %d", 50);
     ALINK_LOG_WARN("iiiiiiiiiiiiii %d", 20);
     ALINK_LOG_INFO("iiiiiiiiiiiiii %d", 400);
+
+    ALINK_ENGINE_ASSERT(false, "test");
   }
-  
+
   void ShutDown() override {
   
   }
