9755fe6d532223cfdaf07050b623bdb846135920
Norbert Bielak <norbertbielak@gmail.com>
2021-02-26 22:36:19 +0100

    Added events

---
+++ src/ALinkEngine/Events/ApplicationEvent.h
@@ -0,0 +1,61 @@
+#ifndef ALINKENGINE_EVENTS_APPLICATIONEVENT_H_
+#define ALINKENGINE_EVENTS_APPLICATIONEVENT_H_
+
+#include "Event.h"
+
+namespace ALinkEngine {
+
+class WindowResizeEvent : public Event {
+ public:
+	WindowResizeEvent(unsigned int width, unsigned int height)
+		: m_Width(width), m_Height(height) {}
+
+	unsigned int GetWidth() const { return m_Width; }
+	unsigned int GetHeight() const { return m_Height; }
+
+	std::string ToString() const override {
+		std::stringstream ss;
+		ss << "WindowResizeEvent: " << m_Width << ", " << m_Height;
+		return ss.str();
+	}
+
+	EVENT_CLASS_TYPE(WindowResize)
+	EVENT_CLASS_CATEGORY(EventCategoryApplication)
+ private:
+	unsigned int m_Width, m_Height;
+};
+
+class WindowCloseEvent : public Event {
+ public:
+	WindowCloseEvent() = default;
+
+	EVENT_CLASS_TYPE(WindowClose)
+	EVENT_CLASS_CATEGORY(EventCategoryApplication)
+};
+
+class AppTickEvent : public Event {
+ public:
+	AppTickEvent() = default;
+
+	EVENT_CLASS_TYPE(AppTick)
+	EVENT_CLASS_CATEGORY(EventCategoryApplication)
+};
+
+class AppUpdateEvent : public Event {
+ public:
+	AppUpdateEvent() = default;
+
+	EVENT_CLASS_TYPE(AppUpdate)
+	EVENT_CLASS_CATEGORY(EventCategoryApplication)
+};
+
+class AppRenderEvent : public Event {
+ public:
+	AppRenderEvent() = default;
+
+	EVENT_CLASS_TYPE(AppRender)
+	EVENT_CLASS_CATEGORY(EventCategoryApplication)
+};
+}
+
+#endif  // ALINKENGINE_EVENTS_APPLICATIONEVENT_H_
---
+++ src/ALinkEngine/Events/KeyEvent.h
@@ -0,0 +1,65 @@
+#ifndef ALINKENGINE_EVENTS_KEYEVENT_H_
+#define ALINKENGINE_EVENTS_KEYEVENT_H_
+
+#include "ALinkEngine/Core/KeyCodes.h"
+#include "Event.h"
+
+namespace ALinkEngine {
+
+class KeyEvent : public Event {
+ public:
+  KeyCode GetKeyCode() const { return m_KeyCode; }
+
+  EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+ protected:
+  explicit KeyEvent(const KeyCode keycode) : m_KeyCode(keycode) {}
+
+  KeyCode m_KeyCode;
+};
+
+class KeyPressedEvent : public KeyEvent {
+ public:
+  KeyPressedEvent(const KeyCode keycode, const uint16_t repeatCount)
+      : KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+  uint16_t GetRepeatCount() const { return m_RepeatCount; }
+
+  std::string ToString() const override {
+    std::stringstream ss;
+    ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount
+       << " repeats)";
+    return ss.str();
+  }
+
+  EVENT_CLASS_TYPE(KeyPressed)
+ private:
+  uint16_t m_RepeatCount;
+};
+
+class KeyReleasedEvent : public KeyEvent {
+ public:
+  explicit KeyReleasedEvent(const KeyCode keycode) : KeyEvent(keycode) {}
+
+  std::string ToString() const override {
+    std::stringstream ss;
+    ss << "KeyReleasedEvent: " << m_KeyCode;
+    return ss.str();
+  }
+
+  EVENT_CLASS_TYPE(KeyReleased)
+};
+
+class KeyTypedEvent : public KeyEvent {
+ public:
+  explicit KeyTypedEvent(const KeyCode keycode) : KeyEvent(keycode) {}
+
+  std::string ToString() const override {
+    std::stringstream ss;
+    ss << "KeyTypedEvent: " << m_KeyCode;
+    return ss.str();
+  }
+
+  EVENT_CLASS_TYPE(KeyTyped)
+};
+}  // namespace ALinkEngine
+#endif  // ALINKENGINE_EVENTS_KEYEVENT_H_
