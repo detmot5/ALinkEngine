3dc7b84e721d01b0f30de17665cf8bbe9605670e
detmot5 <detmot6@gmail.com>
2021-03-02 21:05:32 +0100

    Added example layer

--- src/ALinkSandbox/Sandbox.cpp
+++ src/ALinkSandbox/Sandbox.cpp
@@ -2,6 +2,23 @@
 
 #include <iostream>
 
+
+class ExampleLayer : public ALinkEngine::Layer {
+ public:
+  explicit ExampleLayer(const std::string& name)
+    : Layer(name) {}
+
+  void OnUpdate() override {
+    
+  }
+
+  void OnEvent(ALinkEngine::Event& event) {
+    ALINK_LOG_WARN("Event from layer: %s", event.ToString().c_str());
+  }
+};
+
+
+
 class SandBox : public ALinkEngine::ALinkApplication {
  public:
   void Init() override {
@@ -9,6 +26,7 @@ class SandBox : public ALinkEngine::ALinkApplication {
     ALINK_LOG_ERROR("Testtt %d", 50);
     ALINK_LOG_WARN("iiiiiiiiiiiiii %d", 20);
     ALINK_LOG_INFO("iiiiiiiiiiiiii %d", 400);
+    this->AddLayer(new ExampleLayer("Dupa"));
   }
 
   void ShutDown() override {}
