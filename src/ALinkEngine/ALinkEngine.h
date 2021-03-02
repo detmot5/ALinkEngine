c9727d47663f5d9f79d0816478492947b8334731
detmot5 <detmot6@gmail.com>
2021-03-02 20:59:21 +0100

    Added event header

--- src/ALinkEngine/ALinkEngine.h
+++ src/ALinkEngine/ALinkEngine.h
@@ -1,8 +1,16 @@
 #ifndef ALINKENGINE_ALINKENGINE_H_
 #define ALINKENGINE_ALINKENGINE_H_
 
-#include <Core/Logger.h>
-#include <Core/EntryPoint.h>
-#include <Core/Application.h>
-#include <Core/Core.h>
+#include "Events/KeyEvent.h"
+#include "Events/MouseEvent.h"
+#include "Events/ApplicationEvent.h"
+#include "Events/Event.h"
+
+#include "Core/Logger.h"
+#include "Core/EntryPoint.h"
+#include "Core/Application.h"
+#include "Core/Layer.h"
+#include "Core/Core.h"
+
+
 #endif  // ALINKENGINE_ALINKENGINE_H_
