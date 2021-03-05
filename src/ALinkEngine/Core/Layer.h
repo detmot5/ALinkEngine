71ec787e9caf916b28e37fa1bac48c2737bd5508
detmot5 <detmot6@gmail.com>
2021-03-05 23:15:39 +0100

    Added OnImguiRender

--- src/ALinkEngine/Core/Layer.h
+++ src/ALinkEngine/Core/Layer.h
@@ -15,6 +15,7 @@ class Layer {
   virtual void OnAttach() {}
   virtual void OnDetach() {}
   virtual void OnUpdate() {}
+  virtual void OnImGuiRender() {}
   virtual void OnEvent(Event& event) {}
 
   inline const std::string GetName() const { return debugName; }
