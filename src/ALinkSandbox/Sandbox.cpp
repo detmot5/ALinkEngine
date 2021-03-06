c4b54e614210b15ca78684fd154fcf573d209cf7
detmot5 <detmot6@gmail.com>
2021-03-06 23:34:38 +0100

    Removed test logs

--- src/ALinkSandbox/Sandbox.cpp
+++ src/ALinkSandbox/Sandbox.cpp
@@ -32,11 +32,6 @@ class ExampleLayer : public ALinkEngine::Layer {
 class SandBox : public ALinkEngine::ALinkApplication {
  public:
   void Init() override {
-    
-    ALINK_LOG_WARN("tessst {0}", 20);
-    ALINK_LOG_ERROR("Testtt {0}", 50);
-    ALINK_LOG_WARN("iiiiiiiiiiiiii {0}", 20);
-    ALINK_LOG_INFO("iiiiiiiiiiiiii {0}", 400);
     this->AddLayer(new ExampleLayer("Dupa"));
   }
 
