73739c0f3278bece4d27af339c887aa13437f22b
detmot5 <detmot6@gmail.com>
2021-03-14 18:32:50 +0100

    added shader names handling

--- src/ALinkEngine/GraphicsAPI/OpenGL/OpenGLShader.cpp
+++ src/ALinkEngine/GraphicsAPI/OpenGL/OpenGLShader.cpp
@@ -1,119 +1,38 @@
 #include "alinkpch.h"
-#include "OpenGLShader.h"
 #include <glad/glad.h>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <fstream>
+#include <filesystem>
+
+#include "OpenGLShader.h"
 namespace ALink {
 
-OpenGLShader::OpenGLShader(const std::string& vertexSrc,
-               const std::string& fragmentSrc) {
+static GLenum ShaderTypeFromString(const std::string& type) {
+  if (type == "vertex")                        return GL_VERTEX_SHADER;
+  if (type == "fragment" || type == "pixel")   return GL_FRAGMENT_SHADER;
 
-  // Create an empty vertex shader handle
-  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  ALINK_ENGINE_ASSERT(false, "OpenglShader - ShaderTypeFromString "
+                                            "- Unknown Shader Type");
+  return 0;
+}
 
-  // Send the vertex shader source code to GL
-  // Note that std::string's .c_str is NULL character terminated.
-  const GLchar *source = (const GLchar *)vertexSrc.c_str();
-  glShaderSource(vertexShader, 1, &source, 0);
 
-  // Compile the vertex shader
-  glCompileShader(vertexShader);
+OpenGLShader::OpenGLShader(const std::string& name,
+                           const std::string& vertexSrc,
+                           const std::string& fragmentSrc)
+  : name(name) {
+  std::unordered_map<GLenum, std::string> sources;
+  sources[GL_VERTEX_SHADER] = vertexSrc;
+  sources[GL_FRAGMENT_SHADER] = fragmentSrc;
+  this->Compile(sources);
+}
 
-  GLint isCompiled = 0;
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
-  if (isCompiled == GL_FALSE) {
-    GLint maxLength = 0;
-    glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
-
-    // The maxLength includes the NULL character
-    std::vector<GLchar> infoLog(maxLength);
-    glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
-    ALINK_ENGINE_LOG_ERROR("SHader Compilation error");
-    ALINK_ENGINE_LOG_ERROR("{0}", infoLog.data());
-    
-    // We don't need the shader anymore.
-    glDeleteShader(vertexShader);
-
-    // Use the infoLog as you see fit.
-    
-    // In this simple program, we'll just leave
-    return;
-  }
-
-  // Create an empty fragment shader handle
-  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-  // Send the fragment shader source code to GL
-  // Note that std::string's .c_str is NULL character terminated.
-  source = (const GLchar *)fragmentSrc.c_str();
-  glShaderSource(fragmentShader, 1, &source, 0);
-
-  // Compile the fragment shader
-  glCompileShader(fragmentShader);
-
-  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
-  if (isCompiled == GL_FALSE)
-  {
-    GLint maxLength = 0;
-    glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
-
-    // The maxLength includes the NULL character
-    std::vector<GLchar> infoLog(maxLength);
-    glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
-    ALINK_ENGINE_LOG_ERROR("SHader Link error");
-    ALINK_ENGINE_LOG_ERROR("{0}", infoLog.data());
-    // We don't need the shader anymore.
-    glDeleteShader(fragmentShader);
-    // Either of them. Don't leak shaders.
-    glDeleteShader(vertexShader);
-
-    // Use the infoLog as you see fit.
-    
-    // In this simple program, we'll just leave
-    return;
-  }
-
-  // Vertex and fragment shaders are successfully compiled.
-  // Now time to link them together into a program.
-  // Get a program object.
-  GLuint program = glCreateProgram();
-  this->rendererID = program;
-
-  // Attach our shaders to our program
-  glAttachShader(program, vertexShader);
-  glAttachShader(program, fragmentShader);
-
-  // Link our program
-  glLinkProgram(program);
-
-  // Note the different functions here: glGetProgram* instead of glGetShader*.
-  GLint isLinked = 0;
-  glGetProgramiv(program, GL_LINK_STATUS, (int *)&isLinked);
-  if (isLinked == GL_FALSE) {
-    GLint maxLength = 0;
-    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
-
-    // The maxLength includes the NULL character
-    std::vector<GLchar> infoLog(maxLength);
-    glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
-    ALINK_ENGINE_LOG_ERROR("SHader Compilation error");
-    ALINK_ENGINE_LOG_ERROR("{0}", infoLog.data());
-    // We don't need the program anymore.
-    glDeleteProgram(program);
-    // Don't leak shaders either.
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // Use the infoLog as you see fit.
-    
-    // In this simple program, we'll just leave
-    return;
-  }
-
-  // Always detach shaders after a successful link.
-  glDetachShader(program, vertexShader);
-  glDetachShader(program, fragmentShader);
-
+OpenGLShader::OpenGLShader(const std::string& filePath) {
+  std::string source = this->ReadFile(filePath);
+  auto preProcessedShaderSources = this->PreProcess(source);
+  this->Compile(preProcessedShaderSources);
+  this->name = std::filesystem::path(filePath).stem().string();
 }
 
 OpenGLShader::~OpenGLShader() { glDeleteProgram(this->rendererID); }
@@ -151,6 +70,101 @@ void OpenGLShader::SetUniformMat3(const std::string& name, const glm::mat3& matr
 void OpenGLShader::SetUniformMat4(const std::string& name, const glm::mat4& matrix) {
   GLint location = glGetUniformLocation(this->rendererID, name.c_str());
   glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
+}
+
+void OpenGLShader::Compile(const std::unordered_map<GLenum, std::string>& shaderSources) {
+    // Create an empty vertex shader handle
+  GLuint program = glCreateProgram();
+  std::vector<GLenum> shaderIds;
+  shaderIds.reserve(shaderSources.size());
+  for (auto[type, source] : shaderSources) {
+
+    GLuint shader = glCreateShader(type);
+    const GLchar *sourcePtr = (const GLchar *)source.c_str();
+    glShaderSource(shader, 1, &sourcePtr, 0);
+    // Compile the vertex shader
+    glCompileShader(shader);
+  
+    GLint isCompiled = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+    if (isCompiled == GL_FALSE) {
+      GLint maxLength = 0;
+      glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+      std::vector<GLchar> infoLog(maxLength);
+      glGetShaderInfoLog(shader, maxLength, &maxLength, &infoLog[0]);
+      glDeleteShader(shader);
+      ALINK_ENGINE_ASSERT(false, "Shader compilation error {0}", infoLog.data());
+
+      break;
+    }
+    glAttachShader(program, shader);
+    shaderIds.push_back(shader);
+  }
+  this->rendererID = program;
+
+  glLinkProgram(program);
+
+  // Note the different functions here: glGetProgram* instead of glGetShader*.
+  GLint isLinked = 0;
+  glGetProgramiv(program, GL_LINK_STATUS, reinterpret_cast<int*>(&isLinked));
+  if (isLinked == GL_FALSE) {
+    GLint maxLength = 0;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+    std::vector<GLchar> infoLog(maxLength);
+    glGetProgramInfoLog(program, maxLength, &maxLength, infoLog.data());
+    
+    glDeleteProgram(program);
+  
+    for (auto shaderID : shaderIds) {
+      glDeleteShader(shaderID);
+    }
+
+    ALINK_ENGINE_ASSERT(false, "Shader Link error {0}", infoLog.data());
+    return;
+  }
+
+  // Always detach shaders after a successful link.
+  for (auto shaderID : shaderIds) {
+    glDetachShader(program, shaderID);
+  }
+
+
+}
+
+std::unordered_map<GLenum, std::string> OpenGLShader::PreProcess(const std::string& source) {
+  std::unordered_map<GLenum, std::string> shaderSources;
+  static const char* typeToken = "#type";
+  size_t typeTokenLength = std::strlen(typeToken);
+  size_t pos = source.find(typeToken, 0);
+  while (pos != std::string::npos) {
+    size_t eol = source.find_first_of("\r\n", pos);
+    ALINK_ENGINE_ASSERT(eol != std::string::npos, "Failed to compile shader"
+                                                  " - syntax error in shader type");
+    size_t begin = pos + typeTokenLength + 1;
+    std::string type = source.substr(begin, eol - begin);
+    ALINK_ENGINE_ASSERT(ShaderTypeFromString(type) != 0,
+                        "Failed to compile shader"
+                        "Invalid shader type specification");
+
+    size_t nextLinePos = source.find_first_not_of("\r\n", eol);
+    pos = source.find(typeToken, nextLinePos);
+    shaderSources[ShaderTypeFromString(type)] =
+      source.substr(nextLinePos, pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos));
+  }
+  return shaderSources;
+}
+
+std::string OpenGLShader::ReadFile(const std::string& filePath) {
+  std::ifstream input(filePath, std::ios::in | std::ios::binary);
+  ALINK_ENGINE_ASSERT(input, "Failed to read file");
+
+  std::string result;
+  input.seekg(0, std::ios::end);
+  result.resize(input.tellg());
+  input.seekg(0, std::ios::beg);
+  input.read(result.data(), result.size());
+  input.close();
+  return result;
 }
 
 }
--- src/ALinkEngine/GraphicsAPI/OpenGL/OpenGLShader.h
+++ src/ALinkEngine/GraphicsAPI/OpenGL/OpenGLShader.h
@@ -1,15 +1,18 @@
 #ifndef ALINKENGINE_GRAPHICSAPI_OPENGL_OPENGLSHADER_H_
 #define ALINKENGINE_GRAPHICSAPI_OPENGL_OPENGLSHADER_H_
 #include "Renderer/Shader.h"
+
 #include <glm/glm.hpp>
 namespace ALink {
 
 class OpenGLShader : public Shader {
  public:
-  OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
+  OpenGLShader(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
+  explicit OpenGLShader(const std::string& filePath);
   ~OpenGLShader();
   void Bind() override;
   void Unbind() override;
+
 
   void SetUniformInt(const std::string& name, int value);
   void SetUniformFloat(const std::string& name, float value);
@@ -19,8 +22,14 @@ class OpenGLShader : public Shader {
   void SetUniformMat3(const std::string& name, const glm::mat3& matrix);
   void SetUniformMat4(const std::string& name, const glm::mat4& matrix);
 
+  inline const std::string& GetName() const override { return this->name; };
+ private:
+  void Compile(const std::unordered_map<unsigned int, std::string>& shaderSources);
+  std::unordered_map<unsigned int, std::string> PreProcess(const std::string& source);
+  std::string ReadFile(const std::string& filepath);
  private:
   uint32_t rendererID;
+  std::string name;
 };
 
 }  // namespace ALink
